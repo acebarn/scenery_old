718f706b2183e491c8b43de59012378b3a224284
Sebastian <sebastian.ruziczka@gmail.com>
2013-09-30 07:24:31 +0200

    fertiger  build

--- scenery/protected/views/site/index.php
+++ scenery/protected/views/site/index.php
@@ -50,7 +50,6 @@ $bilder = scandir ( 'images' ); // Den Ordner "images" auslesen
 foreach ( $bilder as $datei ) {
 	if (!is_dir('images/'.$datei) && $datei != "." && $datei != ".."  && $datei != "_notes" && pathinfo($datei)['basename'] != "Thumbs.db" &&  (pathinfo($datei)['extension'] == 'jpg' || pathinfo($datei)['extension'] == 'JPG' )){
 		echo "	<span class=\"block\" >";
-	 
 		echo "		<a href=\"images/" . $datei . "\" class=\"thumbnail\" data-gallery=\"gallery\"><img src=\"images/" . $datei . "\"  width=\"200px\" height=\"200px\" title=\"tololololo\"></a>";
 		echo "	</span>";
 	}
@@ -59,7 +58,7 @@ foreach ( $bilder as $datei ) {
 ?>
 	</div>
 	</div>
-<script>
+	<script>
 document.getElementById('links').onclick = function (event) {
     event = event || window.event;
     var target = event.target || event.srcElement,
