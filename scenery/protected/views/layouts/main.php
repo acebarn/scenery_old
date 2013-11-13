32b770ef00d3e54a5e24633a446e10ede63e5453
Sebastian <sebastian.ruziczka@gmail.com>
2013-11-13 08:52:35 +0100

    Facebook

--- scenery/protected/views/layouts/main.php
+++ scenery/protected/views/layouts/main.php
@@ -24,6 +24,17 @@
 
 <body>
 
+
+
+	<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/de_DE/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <?php 
 //Hier die Java-Skripte registrieren
 $baseUrl = Yii::app()->request->baseUrl;  //Root-Pfad in Variable speichern
@@ -79,9 +90,13 @@ $cs->registerScriptFile($baseUrl.'/js/blueimp-gallery-htmlauslagerung.js');
 	<div class="clear"></div>
 
 	<div id="footer">
+			<div class="fb-like-box" data-href="https://www.facebook.com/SceneryLikeAPicture" data-width="300" data-height="100" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
+	
+	<br>
 		Copyright &copy; <?php echo date('Y'); ?> by scenery.<br/>
 		All Rights Reserved.<br/>
 	</div><!-- footer -->
+
 
 </div><!-- page -->
 </body>
